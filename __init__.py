f9faec8d44a584f75e78bdae56db0e9e01a2ead0
gabaaa <not-set>
2021-05-14 10:48:29 -0500

    Added a user_search and raw_search main functions

--- __init__.py
+++ __init__.py
@@ -2,9 +2,9 @@
 # authors: Gabriel Auger
 # name: wldap
 # licenses: MIT 
-__version__ = "1.0.1"
+__version__ = "2.0.0"
 
-from .dev.wldap import search
+from .dev.wldap import user_search, raw_search, LdapServer
 from .gpkgs import message as msg
 from .gpkgs.options import Options
 from .gpkgs.etconf import Etconf
