98460fbf3db8d8a5d7cff176c9498b7d3a9c51b2
gabaaa <gabaaa.dev@gmail.com>
2025-05-25 08:10:20 -0500

    cleanup for release

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@
 # authors: Gabriel Auger
 # name: wldap
 # licenses: MIT 
-__version__= "3.1.0"
+__version__= "3.1.1"
 
 from .dev.wldap import user_search, raw_search, LdapServer
 from .gpkgs import message as msg
